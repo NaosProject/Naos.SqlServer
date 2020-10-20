3da42e91e0728672d069a9545b6a68c2ccf7f79a
Lawson Caudill <lawson@getthinktank.com>
2020-10-20 01:35:52 +0100

    Added resorce location from Naos.Protocol.SqlServer.

---
+++ Naos.SqlServer.Domain/Model/ResourceLocation/ISqlServerLocator.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ISqlServerLocator.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.SqlServer.Domain
+{
+    /// <summary>
+    /// Generic SQL locator properties to allow for other SQL type locators to share information with the <see cref="SqlOperationsProtocol" />.
+    /// </summary>
+    public interface ISqlServerLocator
+    {
+        /// <summary>
+        /// Gets the name of the server.
+        /// </summary>
+        /// <value>The name of the server.</value>
+        string ServerName { get; }
+
+        /// <summary>
+        /// Gets the name of the database.
+        /// </summary>
+        /// <value>The name of the database.</value>
+        string DatabaseName { get; }
+
+        /// <summary>
+        /// Gets the name of the user.
+        /// </summary>
+        /// <value>The name of the user.</value>
+        string UserName { get; }
+
+        /// <summary>
+        /// Gets the password.
+        /// </summary>
+        /// <value>The password.</value>
+        string Password { get; }
+
+        /// <summary>
+        /// Gets the name of the instance name.
+        /// </summary>
+        /// <value>The name of the instance.</value>
+        string InstanceName { get; }
+
+        /// <summary>
+        /// Gets the port.
+        /// </summary>
+        /// <value>The port.</value>
+        int Port { get; }
+    }
+}
---
+++ Naos.SqlServer.Domain/Model/ResourceLocation/SqlServerLocator.cs
@@ -0,0 +1,96 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SqlServerLocator.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.SqlServer.Domain
+{
+    using System;
+    using Naos.Protocol.Domain;
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Type;
+
+    /// <summary>
+    /// SQL implementation of an <see cref="ResourceLocatorBase" />.
+    /// </summary>
+    public partial class SqlServerLocator : ResourceLocatorBase, ISqlServerLocator, IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SqlServerLocator"/> class.
+        /// </summary>
+        /// <param name="serverName">Name of the server.</param>
+        /// <param name="databaseName">Name of the database.</param>
+        /// <param name="userName">Name of user.</param>
+        /// <param name="password">Password of user.</param>
+        /// <param name="instanceName">Optional name of the instance, default is null.</param>
+        /// <param name="port">Optional port, default is 1433.</param>
+        public SqlServerLocator(
+            string serverName,
+            string databaseName,
+            string userName,
+            string password,
+            string instanceName = null,
+            int port = 1433)
+        {
+            serverName.MustForArg(nameof(serverName)).NotBeNullNorWhiteSpace();
+            databaseName.MustForArg(nameof(databaseName)).NotBeNullNorWhiteSpace();
+            this.ServerName = serverName;
+            this.DatabaseName = databaseName;
+            this.UserName = userName;
+            this.Password = password;
+            this.InstanceName = instanceName;
+            this.Port = port;
+        }
+
+        /// <summary>
+        /// Gets the name of the server.
+        /// </summary>
+        /// <value>The name of the server.</value>
+        public string ServerName { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the database.
+        /// </summary>
+        /// <value>The name of the database.</value>
+        public string DatabaseName { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the user.
+        /// </summary>
+        /// <value>The name of the user.</value>
+        public string UserName { get; private set; }
+
+        /// <summary>
+        /// Gets the password.
+        /// </summary>
+        /// <value>The password.</value>
+        public string Password { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the instance name.
+        /// </summary>
+        /// <value>The name of the instance.</value>
+        public string InstanceName { get; private set; }
+
+        /// <summary>
+        /// Gets the port.
+        /// </summary>
+        /// <value>The port.</value>
+        public int Port { get; private set; }
+
+        /// <summary>
+        /// Builds the invalid stream locator type exception.
+        /// </summary>
+        /// <param name="typeOfLocator">The type of locator.</param>
+        /// <returns>Correct exception.</returns>
+        public static Exception BuildInvalidLocatorException(
+            Type typeOfLocator)
+        {
+            var message = FormattableString.Invariant($"Only {nameof(SqlServerLocator)}'s are supported; provided was: {typeOfLocator}");
+            var result = new NotSupportedException(message);
+
+            return result;
+        }
+    }
+}
