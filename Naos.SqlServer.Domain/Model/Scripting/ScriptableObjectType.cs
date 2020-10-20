4c81afef01c4d5d39d88c9ef2b07cae7d46ae505
Lawson Caudill <lawson@getthinktank.com>
2020-10-20 01:21:34 +0100

    Added scripting containers from Naos.Database.

---
+++ Naos.SqlServer.Domain/Model/Scripting/ScriptableObjectType.cs
@@ -0,0 +1,64 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ScriptableObjectType.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.SqlServer.Domain
+{
+    /// <summary>
+    /// Enumeration of scriptable object types.
+    /// </summary>
+    public enum ScriptableObjectType
+    {
+        /// <summary>
+        /// Invalid default state.
+        /// </summary>
+        Invalid,
+
+        /// <summary>
+        /// Database table.
+        /// </summary>
+        Table,
+
+        /// <summary>
+        /// Table foreign key.
+        /// </summary>
+        ForeignKey,
+
+        /// <summary>
+        /// Table or view index.
+        /// </summary>
+        Index,
+
+        /// <summary>
+        /// Database view.
+        /// </summary>
+        View,
+
+        /// <summary>
+        /// Stored procedure.
+        /// </summary>
+        StoredProcedure,
+
+        /// <summary>
+        /// User defined function.
+        /// </summary>
+        UserDefinedFunction,
+
+        /// <summary>
+        /// User defined data type.
+        /// </summary>
+        UserDefinedDataType,
+
+        /// <summary>
+        /// Database role.
+        /// </summary>
+        DatabaseRole,
+
+        /// <summary>
+        /// Database user.
+        /// </summary>
+        User,
+    }
+}
