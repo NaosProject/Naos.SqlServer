392a4c1d71cde96ff1d4a5640724d31fc7e8cedc
Lawson Caudill <lawson@getthinktank.com>
2020-10-20 01:21:12 +0100

    Added stored procedure parameter representation from Naos.Protocol.SqlServer.

---
+++ Naos.SqlServer.Domain/Model/ParameterRepresentation/SqlOutputParameterRepresentationBase.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SqlOutputParameterRepresentationBase.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.SqlServer.Domain
+{
+    /// <summary>
+    /// Top level base of a SQL Parameter.
+    /// </summary>
+    public abstract partial class SqlOutputParameterRepresentationBase : SqlParameterRepresentationBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SqlOutputParameterRepresentationBase"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="dataType">The type.</param>
+        protected SqlOutputParameterRepresentationBase(
+            string name,
+            SqlDataTypeRepresentationBase dataType)
+            : base(name, dataType)
+        {
+        }
+    }
+}
---
+++ Naos.SqlServer.Domain/Model/ParameterRepresentation/SqlOutputParameterRepresentation{TValue}.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SqlOutputParameterRepresentation{TValue}.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.SqlServer.Domain
+{
+    using System;
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Type.Recipes;
+
+    /// <summary>
+    /// Top level .
+    /// </summary>
+    /// <typeparam name="TValue">Type of the Output value.</typeparam>
+    public partial class SqlOutputParameterRepresentation<TValue> : SqlOutputParameterRepresentationBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SqlOutputParameterRepresentation{TValue}"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="dataType">The type.</param>
+        public SqlOutputParameterRepresentation(
+            string name,
+            SqlDataTypeRepresentationBase dataType)
+            : base(name, dataType)
+        {
+            dataType.MustForTest(nameof(dataType)).NotBeNull();
+
+            var valueType = typeof(TValue);
+            dataType.ValidateObjectTypeIsCompatible(valueType);
+        }
+    }
+}
