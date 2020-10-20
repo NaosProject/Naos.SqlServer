755e5c3f46b54841532947d142a7277f473e565e
Lawson Caudill <lawson@getthinktank.com>
2020-10-20 01:20:33 +0100

    Added schema representation from Naos.Protocol.SqlServer (and non-compiled legacy Naos.Database.SqlServer.Adminsitration).

---
+++ Naos.SqlServer.Domain/Model/SchemaRepresentation/ColumnRepresentation.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ColumnRepresentation.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.SqlServer.Domain
+{
+    using OBeautifulCode.Assertion.Recipes;
+
+    /// <summary>
+    /// Top level base of a SQL Parameter.
+    /// </summary>
+    public class ColumnRepresentation
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColumnRepresentation"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="dataType">Type of the data.</param>
+        public ColumnRepresentation(
+            string name,
+            SqlDataTypeRepresentationBase dataType)
+        {
+            name.MustForArg(nameof(name)).NotBeNull().And().BeAlphanumeric();
+            dataType.MustForArg(nameof(dataType)).NotBeNull();
+
+            this.Name = name;
+            this.DataType = dataType;
+        }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets the type of the data.
+        /// </summary>
+        /// <value>The type of the data.</value>
+        public SqlDataTypeRepresentationBase DataType { get; private set; }
+    }
+}
