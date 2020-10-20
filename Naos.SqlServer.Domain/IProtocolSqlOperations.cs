87c1d48299c018f3c27e76c3ab5fdf865ef4e860
Lawson Caudill <lawson@getthinktank.com>
2020-10-20 01:57:53 +0100

    Final Domain cleanup (pre code gen).

---
+++ Naos.SqlServer.Domain/IProtocolSqlOperations.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IProtocolSqlOperations.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.SqlServer.Domain
+{
+    using Naos.Protocol.Domain;
+
+    /// <summary>
+    /// Protocols for SQL Operations.
+    /// </summary>
+    public interface IProtocolSqlOperations : ISyncAndAsyncReturningProtocol<ExecuteStoredProcedureOp, StoredProcedureExecutionResult>
+    {
+    }
+}
