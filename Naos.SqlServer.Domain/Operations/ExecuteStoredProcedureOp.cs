1720d3fd4bb3f50c6c43edadbd2d27e7318db00e
Lawson Caudill <lawson@getthinktank.com>
2020-10-20 01:43:04 +0100

    Added operations from Naos.Protocol.SqlServer

---
+++ Naos.SqlServer.Domain/Operations/ExecuteStoredProcedureOp.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExecuteStoredProcedureOp.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.SqlServer.Domain
+{
+    using System.Collections.Generic;
+    using Naos.Protocol.Domain;
+    using OBeautifulCode.Assertion.Recipes;
+
+    /// <summary>
+    /// Stored procedure to execute.
+    /// </summary>
+    public partial class ExecuteStoredProcedureOp : ReturningOperationBase<StoredProcedureExecutionResult>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExecuteStoredProcedureOp"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="parameterNameToDetailsMap">The parameter name to details map.</param>
+        public ExecuteStoredProcedureOp(
+            string name,
+            IReadOnlyDictionary<string, SqlParameterRepresentationBase> parameterNameToDetailsMap)
+        {
+            name.MustForArg(nameof(name)).NotBeNullNorWhiteSpace();
+            parameterNameToDetailsMap.MustForArg(nameof(parameterNameToDetailsMap)).NotBeNull();
+
+            this.Name = name;
+            this.ParameterNameToDetailsMap = parameterNameToDetailsMap;
+        }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets the parameter name to details map.
+        /// </summary>
+        /// <value>The parameter name to details map.</value>
+        public IReadOnlyDictionary<string, SqlParameterRepresentationBase> ParameterNameToDetailsMap { get; private set; }
+    }
+}
---
+++ Naos.SqlServer.Domain/Operations/GetIdAddIfNecessarySerializerRepresentationOp.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetIdAddIfNecessarySerializerRepresentationOp.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.SqlServer.Domain
+{
+    using System;
+    using Naos.Protocol.Domain;
+    using Naos.SqlServer.Domain;
+    using OBeautifulCode.Serialization;
+
+    /// <summary>
+    /// Find the identity of a <see cref="SerializerRepresentation"/>.
+    /// </summary>
+    public partial class GetIdAddIfNecessarySerializerRepresentationOp : ReturningOperationBase<int>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetIdAddIfNecessarySerializerRepresentationOp"/> class.
+        /// </summary>
+        /// <param name="resourceLocator">Stream locator to inspect.</param>
+        /// <param name="serializerRepresentation">The serialization description.</param>
+        /// <param name="serializationFormat">The serialization format.</param>
+        /// <exception cref="System.ArgumentNullException">serializerRepresentation.</exception>
+        public GetIdAddIfNecessarySerializerRepresentationOp(
+            SqlServerLocator resourceLocator,
+            SerializerRepresentation serializerRepresentation,
+            SerializationFormat serializationFormat)
+        {
+            this.ResourceLocator = resourceLocator ?? throw new ArgumentNullException(nameof(resourceLocator));
+            this.SerializerRepresentation = serializerRepresentation ?? throw new ArgumentNullException(nameof(serializerRepresentation));
+
+            if (serializationFormat == SerializationFormat.Invalid)
+            {
+                throw new ArgumentException("Format cannot be 'Invalid'.", nameof(serializationFormat));
+            }
+
+            this.SerializationFormat = serializationFormat;
+        }
+
+        /// <summary>
+        /// Gets the serialization description.
+        /// </summary>
+        /// <value>The serialization description.</value>
+        public SerializerRepresentation SerializerRepresentation { get; private set; }
+
+        /// <summary>
+        /// Gets the serialization format.
+        /// </summary>
+        /// <value>The serialization format.</value>
+        public SerializationFormat SerializationFormat { get; private set; }
+
+        /// <summary>
+        /// Gets the stream locator.
+        /// </summary>
+        /// <value>The stream locator.</value>
+        public SqlServerLocator ResourceLocator { get; private set; }
+    }
+}
