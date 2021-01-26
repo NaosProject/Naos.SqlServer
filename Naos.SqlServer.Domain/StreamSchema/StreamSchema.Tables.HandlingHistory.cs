2fdc2ae654c89fb8d042c172df29fcad1ae29b00
Lawson Caudill <lawson@getthinktank.com>
2021-01-26 21:06:47 +0000

    Added additional index to HandlingHistory table.

--- Naos.SqlServer.Domain/StreamSchema/StreamSchema.Tables.HandlingHistory.cs
+++ Naos.SqlServer.Domain/StreamSchema/StreamSchema.Tables.HandlingHistory.cs
@@ -134,6 +134,14 @@ CREATE NONCLUSTERED INDEX [IX_{nameof(HandlingHistory)}_{nameof(Status)}_Asc] ON
 	[{nameof(Status)}] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 
+CREATE NONCLUSTERED INDEX [IX_{nameof(HandlingHistory)}_{nameof(RecordId)}_{nameof(Concern)}_Asc] ON [{streamName}].[{Table.Name}]
+(
+	[{nameof(RecordId)}] DESC,
+	[{nameof(Concern)}] ASC
+)
+INCLUDE ([{nameof(Id)}])
+WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
 			");
 
                     return result;
