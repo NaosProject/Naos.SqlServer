cdf523aa0b9af76c147de4ff9750dbcf3f422419
Lawson Caudill <lawson@getthinktank.com>
2021-02-08 16:54:29 +0000

    Added support to update stored procedures and return the prior version that existed.

---
+++ Naos.SqlServer.Domain/Operations/UpdateStreamStoredProceduresOp.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UpdateStreamStoredProceduresOp.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.SqlServer.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Naos.CodeAnalysis.Recipes;
+    using Naos.Database.Domain;
+    using Naos.Protocol.Domain;
+    using Naos.SqlServer.Domain;
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Collection.Recipes;
+    using OBeautifulCode.Representation.System;
+    using OBeautifulCode.Serialization;
+    using OBeautifulCode.Type;
+    using static System.FormattableString;
+
+    /// <summary>
+    /// Alters the stored procedures of the stream to the schema version being executed.
+    /// </summary>
+    public partial class UpdateStreamStoredProceduresOp : ReturningOperationBase<UpdateStreamStoredProceduresResult>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateStreamStoredProceduresOp"/> class.
+        /// </summary>
+        public UpdateStreamStoredProceduresOp()
+        {
+        }
+    }
+}
--- Naos.SqlServer.Protocol.Client/Stream/SqlStream.CreateOrUpdate.cs
+++ Naos.SqlServer.Protocol.Client/Stream/SqlStream.CreateOrUpdate.cs
@@ -7,14 +7,17 @@
 namespace Naos.SqlServer.Protocol.Client
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using Naos.Database.Domain;
     using Naos.Protocol.Domain;
     using Naos.SqlServer.Domain;
+    using OBeautifulCode.Collection.Recipes;
     using OBeautifulCode.Database.Recipes;
     using static System.FormattableString;
 
-    public partial class SqlStream
+    public partial class SqlStream : IReturningProtocol<UpdateStreamStoredProceduresOp, UpdateStreamStoredProceduresResult>
     {
         /// <inheritdoc />
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling", Justification = "Acceptable given it creates the streams.")]
@@ -99,6 +102,76 @@ namespace Naos.SqlServer.Protocol.Client
 
             var createResult = new CreateStreamResult(alreadyExisted, wasCreated);
             return createResult;
+        }
+
+        /// <inheritdoc />
+        public UpdateStreamStoredProceduresResult Execute(
+            UpdateStreamStoredProceduresOp operation)
+        {
+            var allLocators = this.ResourceLocatorProtocols.Execute(new GetAllResourceLocatorsOp());
+            var priorVersions = new List<string>();
+            foreach (var locator in allLocators)
+            {
+                if (locator is SqlServerLocator sqlLocator)
+                {
+                    using (var connection = sqlLocator.OpenSqlConnection(this.DefaultConnectionTimeout))
+                    {
+                        var streamAlreadyExists = connection.HasAtLeastOneRowWhenReading(
+                            Invariant($"select * from sys.schemas where name = '{this.Name}'"));
+
+                        if (!streamAlreadyExists)
+                        {
+                            throw new InvalidDataException(
+                                FormattableString.Invariant(
+                                    $"Stream '{this.Name}' did exist, this is unexpected and not possible to ALTER stored procedures as they are not present."));
+                        }
+
+                        var alterScripts = new[]
+                                           {
+                                               StreamSchema.Funcs.GetTagsTableVariableFromTagsXml.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Funcs.GetTagsTableVariableFromTagIdsXml.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.GetIdAddIfNecessaryTypeWithoutVersion.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.GetIdAddIfNecessaryTypeWithVersion.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.GetTypeFromId.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.GetIdAddIfNecessarySerializerRepresentation.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.GetSerializerRepresentationFromId.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.GetIdsAddIfNecessaryTagSet.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.GetTagSetFromIds.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.GetLatestRecordMetadataById.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.GetLatestRecordById.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.GetNextUniqueLong.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.PutRecord.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.PutHandling.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.GetCompositeHandlingStatus.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.TryHandleRecord.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.CreateStreamUser.BuildCreationScript(this.Name, true),
+                                               StreamSchema.Sprocs.GetStreamDetails.BuildCreationScript(this.Name, true),
+                                           };
+
+                        var detailsOperation = StreamSchema.Sprocs.GetStreamDetails.BuildExecuteStoredProcedureOp(this.Name);
+                        var protocol = this.BuildSqlOperationsProtocol(sqlLocator);
+                        var sprocResult = protocol.Execute(detailsOperation);
+                        var detailsXml = sprocResult.OutputParameters[StreamSchema.Sprocs.GetStreamDetails.OutputParamName.DetailsXml.ToString()].GetValue<string>();
+                        var detailsMap = TagConversionTool.GetTagsFromXmlString(detailsXml);
+                        var version = detailsMap?.FirstOrDefault(_ => _.Key == StreamSchema.Sprocs.GetStreamDetails.VersionKey).Value;
+                        priorVersions.Add(version);
+
+                        foreach (var script in alterScripts)
+                        {
+                            connection.ExecuteNonQuery(script);
+                        }
+                    }
+                }
+                else
+                {
+                    throw SqlServerLocator.BuildInvalidLocatorException(locator.GetType());
+                }
+            }
+
+            var distinctNonNullVersions = priorVersions.Distinct().Where(_ => _ != null).ToList();
+            var priorVersion = distinctNonNullVersions.Any() ? distinctNonNullVersions.ToCsv() : null;
+            var updateResult = new UpdateStreamStoredProceduresResult(priorVersion);
+            return updateResult;
         }
     }
 }
