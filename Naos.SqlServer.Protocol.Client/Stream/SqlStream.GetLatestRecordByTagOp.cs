a9ca41fdf3beb57e444df93c89ba4fed800a8b64
Lawson Caudill <lawson@getthinktank.com>
2021-07-26 20:47:24 -0400

    Updated Naos.Database and added method for GetLatestRecordByTagOp (not implemented, just throwing notimplemented).

---
+++ Naos.SqlServer.Protocol.Client/Stream/SqlStream.GetLatestRecordByTagOp.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SqlStream.GetLatestRecordByTagOp.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.SqlServer.Protocol.Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using System.Linq;
+    using Naos.CodeAnalysis.Recipes;
+    using Naos.Database.Domain;
+    using Naos.SqlServer.Domain;
+    using OBeautifulCode.DateTime.Recipes;
+    using OBeautifulCode.Serialization;
+    using OBeautifulCode.String.Recipes;
+    using static System.FormattableString;
+
+    public partial class SqlStream
+    {
+        /// <inheritdoc />
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "internalRecordId", Justification = "Leaving for debugging and future use.")]
+        public override StreamRecord Execute(
+            GetLatestRecordByTagOp operation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
