7f0d0ec40e4da1947d9252f5d1bfc5a0b4eca408
Lawson Caudill <lawson@getthinktank.com>
2022-11-04 00:09:05 -0400

    Added direct async support for SqlOperationsProtocol.ExecuteAsync(ExecuteStoredProcedureOp).

--- Naos.SqlServer.Protocol.Client/Protocols/SqlOperationsProtocol.ExecuteSproc.cs
+++ Naos.SqlServer.Protocol.Client/Protocols/SqlOperationsProtocol.ExecuteSproc.cs
@@ -35,24 +35,8 @@ namespace Naos.SqlServer.Protocol.Client
                     command.CommandType = CommandType.StoredProcedure;
 
                     var outputParameters = new List<Tuple<SqlParameter, OutputParameterDefinitionBase>>();
-                    foreach (var parameterRepresentation in operation.Parameters)
-                    {
-                        var parameter = parameterRepresentation.ToSqlParameter();
-
-                        if (parameter.Direction == ParameterDirection.Output)
-                        {
-                            if (parameterRepresentation is OutputParameterDefinitionBase outputParameterRepresentation)
-                            {
-                                outputParameters.Add(new Tuple<SqlParameter, OutputParameterDefinitionBase>(parameter, outputParameterRepresentation));
-                            }
-                            else
-                            {
-                                throw new NotSupportedException(FormattableString.Invariant($"Cannot have a {nameof(SqlParameter)} with {nameof(SqlParameter.Direction)} equal to {nameof(ParameterDirection.Output)} and the representation not be a {nameof(OutputParameterDefinitionBase)}, it was a {parameterRepresentation.GetType().ToStringReadable()}."));
-                            }
-                        }
-
-                        command.Parameters.Add(parameter);
-                    }
+                    var parameterDefinitions = operation.Parameters;
+                    LoadParametersIntoCommand(parameterDefinitions, outputParameters, command);
 
                     command.ExecuteNonQuery();
 
@@ -60,7 +44,7 @@ namespace Naos.SqlServer.Protocol.Client
                     {
                         var outputParameterWithResult = outputParameter.Item2.CreateResult(outputParameter.Item1.Value);
 
-                        outputParametersWithExecutionResult.Add(outputParameter.Item2.Name,  outputParameterWithResult);
+                        outputParametersWithExecutionResult.Add(outputParameter.Item2.Name, outputParameterWithResult);
                     }
                 }
             }
@@ -71,11 +55,61 @@ namespace Naos.SqlServer.Protocol.Client
         }
 
         /// <inheritdoc />
-        public Task<StoredProcedureExecutionResult> ExecuteAsync(
+        public async Task<StoredProcedureExecutionResult> ExecuteAsync(
             ExecuteStoredProcedureOp operation)
         {
-            // TODO: mirror fully ASYNC code here
-            throw new System.NotImplementedException();
+            var outputParametersWithExecutionResult = new Dictionary<string, ISqlOutputParameterResult>();
+            using (var sqlConnection = this.sqlServerLocator.OpenSqlConnection(this.defaultConnectionTimeout))
+            {
+                using (var command = sqlConnection.BuildSqlCommand(operation.Name, (int)this.defaultCommandTimeout.TotalSeconds))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    var outputParameters = new List<Tuple<SqlParameter, OutputParameterDefinitionBase>>();
+                    var parameterDefinitions = operation.Parameters;
+                    LoadParametersIntoCommand(parameterDefinitions, outputParameters, command);
+
+                    await command.ExecuteNonQueryAsync();
+
+                    foreach (var outputParameter in outputParameters)
+                    {
+                        var outputParameterWithResult = outputParameter.Item2.CreateResult(outputParameter.Item1.Value);
+
+                        outputParametersWithExecutionResult.Add(outputParameter.Item2.Name, outputParameterWithResult);
+                    }
+                }
+            }
+
+            var result = new StoredProcedureExecutionResult(operation, outputParametersWithExecutionResult);
+
+            return result;
+        }
+
+        private static void LoadParametersIntoCommand(
+            IReadOnlyList<ParameterDefinitionBase> parameterDefinitions,
+            List<Tuple<SqlParameter, OutputParameterDefinitionBase>> outputParameters,
+            SqlCommand command)
+        {
+            foreach (var parameterRepresentation in parameterDefinitions)
+            {
+                var parameter = parameterRepresentation.ToSqlParameter();
+
+                if (parameter.Direction == ParameterDirection.Output)
+                {
+                    if (parameterRepresentation is OutputParameterDefinitionBase outputParameterRepresentation)
+                    {
+                        outputParameters.Add(new Tuple<SqlParameter, OutputParameterDefinitionBase>(parameter, outputParameterRepresentation));
+                    }
+                    else
+                    {
+                        throw new NotSupportedException(
+                            FormattableString.Invariant(
+                                $"Cannot have a {nameof(SqlParameter)} with {nameof(SqlParameter.Direction)} equal to {nameof(ParameterDirection.Output)} and the representation not be a {nameof(OutputParameterDefinitionBase)}, it was a {parameterRepresentation.GetType().ToStringReadable()}."));
+                    }
+                }
+
+                command.Parameters.Add(parameter);
+            }
         }
     }
 }
