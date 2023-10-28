caf6f869a2d5ed2973e55a1186dc8cc3658d4284
Lawson Caudill <1660562+wlscaudill@users.noreply.github.com>
2023-10-28 17:47:16 -0400

    Work in progress for some management features.

---
+++ Naos.SqlServer.Domain/Logic/ManagementSchema/ManagementSchema.Sprocs.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ManagementSchema.Sprocs.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.SqlServer.Domain
+{
+    using System.Diagnostics.CodeAnalysis;
+    using Naos.CodeAnalysis.Recipes;
+
+    public static partial class ManagementSchema
+    {
+        /// <summary>
+        /// Stored procedures.
+        /// </summary>
+        [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Sprocs", Justification = NaosSuppressBecause.CA1704_IdentifiersShouldBeSpelledCorrectly_SpellingIsCorrectInContextOfTheDomain)]
+        public static partial class Sprocs
+        {
+        }
+    }
+}
---
+++ Naos.SqlServer.Domain/Logic/ManagementSchema/ManagementSchema.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ManagementSchema.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.SqlServer.Domain
+{
+    using System.Collections.Generic;
+    using static System.FormattableString;
+
+    /// <summary>
+    /// Container for schema used to service database.
+    /// </summary>
+    public static partial class ManagementSchema
+    {
+    }
+}
