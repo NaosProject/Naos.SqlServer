6e27f4195256c128b4c403c9acf057b719f0d7f5
Suraj Gupta <SurajGupta@users.noreply.github.com>
2026-05-11 13:00:19 -0400

    seed Protocol.Validation project and update nuget packages

---
+++ Naos.SqlServer.Protocol.Validation/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Naos.SqlServer.Protocol.Validation")]
+[assembly: AssemblyDescription("Naos.SqlServer.Protocol.Validation")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Naos Project")]
+[assembly: AssemblyProduct("Naos.SqlServer.Protocol.Validation")]
+[assembly: AssemblyCopyright("Copyright © Naos Project 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("09405775-1c20-4fa9-a600-edadc0afa835")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
